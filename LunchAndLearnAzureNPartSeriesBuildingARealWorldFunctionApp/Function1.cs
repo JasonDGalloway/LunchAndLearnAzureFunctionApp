01cf4c5a089b12ddbdfd5cb86684cf7bdfc2bae8
Jason D Galloway <accounts@gallowayzoo.net>
2020-12-15 13:11:44 -0600

    shorten solution name

--- LunchAndLearnAzureNPartSeriesBuildingARealWorldFunctionApp/Function1.cs
+++ LunchAndLearnAzureNPartSeriesBuildingARealWorldFunctionApp/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace LunchAndLearnAzureNPartSeriesBuildingARealWorldFunctionApp
+namespace LunchAndLearnAzureFunctionApp
 {
     public static class Function1
     {
