5a9a153becf9aa44f0b5431e0abc529c4855be6e
Jason D Galloway <accounts@gallowayzoo.net>
2020-12-15 18:22:43 -0600

    Refactor Services

---
+++ LunchAndLearnAzureFunctionApp/Services/HealthCheckService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace LunchAndLearnAzureFunctionApp.Services
+{
+    public class HealthCheckService : IHealthCheckService
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return await Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
