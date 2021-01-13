b72ca6c18712d3001a3a2a6744715fa3d746b796
Jason Galloway <jgalloway@apexsystems.com>
2021-01-13 17:39:26 -0600

    remove if then logic

--- LunchAndLearnAzureFunctionApp/Services/NorthAmericanFootballTeamsService.cs
+++ LunchAndLearnAzureFunctionApp/Services/NorthAmericanFootballTeamsService.cs
@@ -20,20 +20,13 @@ namespace LunchAndLearnAzureFunctionApp.Services
 
         public List<string> GetTeamNames(string city)
         {
-            if (_footballSettings.Leagues.Any(x => x.Acronym == _footballSettings.SelectedLeague) == false)
-            {
-                throw new ArgumentException("invalid league selected");
-            }
+            var list = _footballRepository
+                .GetCityTeamNames()
+                .Where(x => x.Key.ToUpper() == city.ToUpper())
+                .Select(x => x.Value)
+                .ToList();
 
-            if (_footballSettings.SelectedLeague == "NFL")
-            {
-                var list = _footballRepository.GetCityTeamNames()
-                    .Where(x => x.Key.ToUpper() == city.ToUpper()).Select(x => x.Value).ToList();
-                return list.Count == 0 ? new List<string> { "Sorry, no team.  Maybe your city can get the Jets cheap..." } : list;
-            }
-            
-            return new List<string> { $"Sorry your league {_footballSettings.SelectedLeague} is not available." };
-
+            return list.Count == 0 ? new List<string> { "Sorry, no team.  Maybe your city can get the Jets cheap..." } : list;
         }
 
         public List<string> GetTeamNames() => _footballRepository.GetCityTeamNames().Select(x => x.Value).ToList();
